9580b9faa5f319de48f664860500a2fa9279c59f
TeacherTay <133699296+TeacherTay@users.noreply.github.com>
2024-11-25 19:49:58 -0300

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,230 @@
+MOVIES.JS 
+export const movies = [ { 
+title: "Donzela", 
+genre: "Ação", 
+ageRating: Livre, 
+synopsis: "Estrelada por Mille Bobby-Brown, o filme Donzela segue a personagem Elodie (Bobby Brown), uma princesa de um reino, que concorda em se casar com um lindo príncipe de outro lugar. Mas o que ela não sabe, é que este conto de fadas é na realidade, um grande engano. A realeza pretende sacrificá-la. Enganada pelo rei, a princesa se torna o sacrifício de uma antiga dívida. Agora, presa em uma caverna de dragão e sabendo que ninguém vem resgatá-la, ela precisará usar sua astúcia, inteligência e perseverança para escapar com vida - tudo isso, sem a ajuda de nenhum príncipe encantado.
+", 
+trailer: "https://youtu.be/WvFHERDoBVg?si=tW1u19KInn26mP_Q" 
+}, 
+{ 
+title: ‘’A Fera Do Mar", 
+genre: "Ação", 
+ageRating: 10, 
+synopsis: "Em A Fera do Mar, um charmoso marinheiro deixa o mundo confortável do continente para navegar em águas desconhecidas. Durante a aventura ele se depara com um intrigante monstro marinho que, surpreendentemente, acaba se tornando um aliado essencial no enfrentamento das tormentas.  A vida do lendário caçador de monstros marinhos é virada de cabeça para baixo a partir dessa amizade - com a fera mais perigosa de todas.
+", 
+trailer: "https://youtu.be/e-KPygLf5TA?si=VDFUr3Mor15ra7wgB
+" 
+}, 
+{ 
+title: "Zootopia", 
+genre: "Ação", 
+ageRating: 12, 
+synopsis: "Em Zootopia: Essa Cidade é o Bicho, acompanhamos a história de Judy Hopps, uma pequena fazendeira que é filha de agricultores. Insatisfeita com a vida no interior, ela tem sonhos maiores: se mudar para a cidade grande, Zootopia, e se tornar a primeira coelha policial. Quando Judy consegue alcançar o seu objetivo, ela é designada para a sua primeira e grande missão, que é encontrar um animal perdido. Contando com a ajuda inesperada de Nick, uma raposa conhecida por sua malícia e infrações, ela descobre que existe uma conspiração que afetará toda a cidade.
+", 
+trailer: "https://youtu.be/prct6AB5tR8?si=cGj5R6z5ZRBU0kEk
+" 
+}, 
+{ 
+title: "Batman:O Cavaleiro Das Trevas", 
+genre: "Ação", 
+ageRating: 14, 
+synopsis: "Após dois anos desde o surgimento do Batman (Christian Bale), os criminosos de Gotham City têm muito o que temer. Com a ajuda do tenente James Gordon (Gary Oldman) e do promotor público Harvey Dent (Aaron Eckhart), Batman luta contra o crime organizado. Acuados com o combate, os chefes do crime aceitam a proposta feita pelo Coringa (Heath Ledger) e o contratam para combater o Homem-Morcego.
+", 
+trailer: "https://youtu.be/eq5IYvQkht4?si=L14Irp7FaUN3BqLV
+" 
+}, 
+{ 
+title: "Monstros S.A", 
+genre: "Comédia", 
+ageRating: Livre, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "O diário de um banana", 
+genre: "Comédia", 
+ageRating: 10, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "O pequenino",
+genre: "Comédia", 
+ageRating: 12, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Assassino por Acaso", 
+genre: "Comédia", 
+ageRating: 14, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Todo Mundo Em Pânico", 
+genre: "Comédia", 
+ageRating: 16, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "A Forja", 
+genre: "Drama", 
+ageRating: livre, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Superação: O milagre da fé", 
+genre: "Drama", 
+ageRating: 10, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "2 Corações", 
+genre: "Drama", 
+ageRating: 12, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "O Banquete", 
+genre: "Drama", 
+ageRating: 14, synopsis: "", 
+trailer: "" 
+},
+{ 
+title: "O regresso", 
+genre: "Drama", 
+ageRating: 16, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "O Espanta Tubarões", 
+genre: "Animação", 
+ageRating: livre, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Coraline", 
+genre: "Animação", 
+ageRating: 10, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "O Menino e a Garça", 
+genre: "Animação", 
+ageRating: 12, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Voltando do inferno", 
+genre: "Animação", 
+ageRating: 14, 
+synopsis: "", 
+trailer: "" 
+} 
+
+
+{ 
+title: "Festa da Salsisha", 
+genre: "Animação", 
+ageRating: 16, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Harry e a Câmara Secreta", 
+genre: "Suspense", 
+ageRating: livre, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "A Bússola de Ouro",
+genre: "Suspense", 
+ageRating: 10, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "A cidade das crianças", 
+genre: "Suspense", 
+ageRating: 12, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Corra!", 
+genre: "Suspense", 
+ageRating: 14, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "A Garota Exemplar", 
+genre: "Suspense", 
+ageRating: 16, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "HALLOWEENTOWN", 
+genre: "Terror", 
+ageRating: livre, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "A casa monstro", 
+genre: "Terror", 
+ageRating: 10, synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "A Família Addams", 
+genre: "Terror", 
+ageRating: 12, synopsis: "", 
+trailer: "" 
+},
+{ 
+title: "Os Fantasmas Ainda Se Divertem", 
+genre: "Terror", 
+ageRating: 14, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "", 
+genre: "Terror", 
+ageRating: 16, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Coraline", 
+genre: "Animação", 
+ageRating: 10, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "O Menino e a Garça", 
+genre: "Animação", 
+ageRating: 12, 
+synopsis: "", 
+trailer: "" 
+}, 
+{ 
+title: "Voltando do inferno", 
+genre: "Animação", 
+ageRating: 14, 
+synopsis: "", 
+trailer: "" 
+} 
+
+
+
+
+
+
+
+
+
+];
